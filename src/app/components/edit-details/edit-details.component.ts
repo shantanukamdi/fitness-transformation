32124d678595457bd01ced7b175abca73896f3d8
Shantanu Kamdi <kamdishantanu@gmail.com>
2017-11-02 17:17:09 +0530

    Add weekly save function:Partial

--- src/app/components/edit-details/edit-details.component.ts
+++ src/app/components/edit-details/edit-details.component.ts
@@ -6,13 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-details.component.css']
 })
 export class EditDetailsComponent implements OnInit {
+  weekNumber: string;
+  exercise: string;
+  bodyFatPercentage: number;
+  waistCircumference: number;
+  weight: number;
+  basalMetabolicRate: number;
+  weeklyImage: FileList;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  detectFiles(event){
+    this.weeklyImage = event.target.files;
+  }
+
   saveUserPersonalData(){
-    
+    let userWeeklyData = {
+      weekNumber: this.weekNumber,
+      exercise: this.exercise,
+      bodyFatPercentage: this.bodyFatPercentage,
+      waistCircumference: this.waistCircumference,
+      weight: this.weight,
+      basalMetabolicRate: this.basalMetabolicRate,
+      weeklyImage: this.weeklyImage.item(0)
+    };
+    //console.log(this.weeklyImage.item(0));
+    console.log(userWeeklyData);
   }
 }
--- src/app/services/firebase-database.service.ts
+++ src/app/services/firebase-database.service.ts
@@ -17,4 +17,7 @@ export class FirebaseDatabaseService {
                           userid: userPersonalData
                        });
   }
+  storeWeeklyData( userWeeklyData ){
+
+  }
 }
